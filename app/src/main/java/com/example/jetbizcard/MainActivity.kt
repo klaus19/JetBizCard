5cc413700123f734257a255c75abbfdf2d6c2e12
tejaskhartude <tejaskhartude@gmail.com>
2022-05-25 14:35:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -0,0 +1,63 @@
+ package com.example.jetbizcard
+
+import android.graphics.Color
+import android.graphics.Color.*
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetbizcard.ui.theme.JetBizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetBizCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                   createBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun createBizCard(){
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            backgroundColor =androidx.compose.ui.graphics.Color.White,
+        elevation = 4.dp
+        ) {
+
+
+        }
+        
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetBizCardTheme {
+          createBizCard()
+    }
+}
